81a571d3c08f5085a2b0f9a933a9afb820e69e11
dctmfoo <nags0107@outlook.com>
2026-03-13 03:38:49 +0530

    Customize Homebrew distribution for dctmfoo

--- packaging/homebrew/Casks/simpalarm.rb
+++ packaging/homebrew/Casks/simpalarm.rb
@@ -2,10 +2,10 @@ cask "simpalarm" do
   version "0.1.0"
   sha256 "73f97948a639a13c664096415dd3429c8068b7355424ae85302f02b5af5dec09"
 
-  url "https://github.com/REPLACE_ME/simpalarm/releases/download/v#{version}/SimpAlarm-#{version}.zip"
+  url "https://github.com/dctmfoo/simpalarm/releases/download/v#{version}/SimpAlarm-#{version}.zip"
   name "SimpAlarm"
   desc "Menu bar alarm app for macOS"
-  homepage "https://github.com/REPLACE_ME/simpalarm"
+  homepage "https://github.com/dctmfoo/simpalarm"
 
   depends_on macos: ">= :sonoma"
 
